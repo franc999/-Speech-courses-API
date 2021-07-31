e66de40748aa61fbf7f9ba00cfe194c8e3764dab
Franco <48919368+franc999@users.noreply.github.com>
2021-07-31 09:16:59 -0300

    asd

--- ManuelsBackEnd/src/main/java/com/manuels/principal/controller/LessonController.java
+++ ManuelsBackEnd/src/main/java/com/manuels/principal/controller/LessonController.java
@@ -25,7 +25,8 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RequestMapping("/api/lessons")
 @RestController
-@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})
+/*@CrossOrigin(origins = "*", methods= {RequestMethod.GET, RequestMethod.POST, RequestMethod.PUT, RequestMethod.DELETE})*/
+@CrossOrigin(origins = "http://localhost:3000")
 public class LessonController {
 
     @Autowired
