9190353b4110ca7cef8061f7ea49dc03c2c4a69a
franc999 <franconocc@gmail.com>
2021-08-04 09:28:34 -0300

    4-8

--- ManuelsBackEnd/src/main/java/com/manuels/principal/ManuelsLocucionApplication.java
+++ ManuelsBackEnd/src/main/java/com/manuels/principal/ManuelsLocucionApplication.java
@@ -13,18 +13,4 @@ public class ManuelsLocucionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManuelsLocucionApplication.class, args);
 	}
-        
-        @Bean
-        public WebMvcConfigurer corsConfigurer(){
-            return new WebMvcConfigurer(){
-                @Override
-                public void addCorsMappings(CorsRegistry registry){
-                    registry
-                            .addMapping("/**")
-                            .allowedOrigins("http://localhost:3000")
-                            .allowedMethods("*")
-                            .allowedHeaders("*");
-                }
-            };
-        }
 }
--- ManuelsBackEnd/src/main/java/com/manuels/principal/security/SecurityConfig.java
+++ ManuelsBackEnd/src/main/java/com/manuels/principal/security/SecurityConfig.java
@@ -13,6 +13,7 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
@@ -35,7 +36,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     
     @Override
     protected void configure(HttpSecurity http) throws Exception{
-        http.csrf().disable()
+        http.cors().and().csrf().disable()
                 .authorizeRequests()
                 .antMatchers("/private/authenticate").permitAll()
                 .antMatchers(HttpMethod.GET, "/api/lessons").permitAll()
@@ -68,7 +69,14 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
         http.addFilterBefore(jwtRequestFilter, UsernamePasswordAuthenticationFilter.class);
     }
+    
+    public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedOrigins("*")
+		.allowedMethods("HEAD", "GET", "PUT", "POST",
+		"DELETE", "PATCH").allowedHeaders("*");
+    }
 
+    
     @Bean
     @Override
     public AuthenticationManager authenticationManagerBean() throws Exception {
