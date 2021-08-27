cf65176f07f3ea8128944a38228c0b18fed0a7a1
franc999 <franconocc@gmail.com>
2021-08-27 15:38:42 -0300

    hello

--- ManuelsBackEnd/src/main/java/com/manuels/principal/ManuelsLocucionApplication.java
+++ ManuelsBackEnd/src/main/java/com/manuels/principal/ManuelsLocucionApplication.java
@@ -2,6 +2,8 @@ package com.manuels.principal;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @SpringBootApplication
 public class ManuelsLocucionApplication {
@@ -9,4 +11,9 @@ public class ManuelsLocucionApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ManuelsLocucionApplication.class, args);
 	}
+        
+        @RequestMapping("/hello")
+        public String hello(){
+            return "HELLOO O OO  ";
+        }
 }
